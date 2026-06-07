8ea5c656921a07de5220b2fc8cc88c608739c12d
SophearithSaing <sophearithsaing123@gmail.com>
2026-06-07 20:06:51 +0700

    Remove topic category from topics module

--- src/topics/topics.module.ts
+++ src/topics/topics.module.ts
@@ -1,20 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Topic, TopicSchema } from './schemas/topic.schema';
-import {
-  TopicCategory,
-  TopicCategorySchema,
-} from './schemas/topic-category.schema';
 import { TopicsService } from './topics.service';
 import { TopicsController } from './topics.controller';
 import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([
-      { name: Topic.name, schema: TopicSchema },
-      { name: TopicCategory.name, schema: TopicCategorySchema },
-    ]),
+    MongooseModule.forFeature([{ name: Topic.name, schema: TopicSchema }]),
   ],
   providers: [TopicsService, RolesGuard],
   controllers: [TopicsController],
