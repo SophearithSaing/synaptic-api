6422e5c8d65c5f67750e4bc05a5d3d14cfe1e03e
SophearithSaing <sophearithsaing123@gmail.com>
2026-06-25 21:01:18 +0700

    Increase body limit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 /**
  * Starts the NestJS API server.
  */
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useBodyParser('json', { limit: '5mb' });
+  app.useBodyParser('urlencoded', { limit: '5mb', extended: true });
 
   app.enableCors({
     origin: process.env.CLIENT_URL ?? 'http://localhost:4200',
