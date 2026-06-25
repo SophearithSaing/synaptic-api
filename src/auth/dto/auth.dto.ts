2ec719f24c731dc93e4178e542c963a87ac6ec70
SophearithSaing <sophearithsaing123@gmail.com>
2026-06-25 21:11:59 +0700

    Add additional input validations

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,24 +1,43 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { Transform } from 'class-transformer';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { normalizeEmailInput, trimInput } from './auth-input-transformers';
 
 export class RegisterDto {
   @IsString()
   @IsNotEmpty()
   @MinLength(3)
+  @MaxLength(32)
+  @Matches(/^[a-zA-Z0-9_.-]+$/)
+  @Transform(trimInput)
   username: string;
 
   @IsEmail()
+  @MaxLength(254)
+  @Transform(normalizeEmailInput)
   email: string;
 
   @IsNotEmpty()
   @MinLength(8)
+  @MaxLength(72)
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/)
   password: string;
 }
 
 export class LoginDto {
   @IsEmail()
+  @MaxLength(254)
+  @Transform(normalizeEmailInput)
   email: string;
 
   @IsNotEmpty()
   @MinLength(8)
+  @MaxLength(72)
   password: string;
 }
