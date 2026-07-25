0fc045ba87c32bc228153de2b930a56aa14e7f8e
SophearithSaing <sophearithsaing123@gmail.com>
2026-07-25 16:28:48 +0700

    Update live question schema to accept answer

--- src/sessions/schemas/live-question.schema.ts
+++ src/sessions/schemas/live-question.schema.ts
@@ -1,9 +1,15 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument, Types } from 'mongoose';
 import type { Question } from '../../questions/schemas/question-set.schema';
+import type { Answer } from './set-attempt.schemas';
 import { LiveSession } from './live-session.schema';
 
 export type LiveQuestionDocument = HydratedDocument<LiveQuestion>;
+
+export enum LiveQuestionStatus {
+  Pending = 'pending',
+  Accepted = 'accepted',
+}
 
 @Schema({ timestamps: true, collection: 'liveQuestions' })
 export class LiveQuestion {
@@ -12,6 +18,21 @@ export class LiveQuestion {
 
   @Prop({ type: Object, required: true })
   question: Question;
+
+  @Prop({ required: true })
+  level: number;
+
+  @Prop({ required: true })
+  questionNumber: number;
+
+  @Prop({ required: true, enum: Object.values(LiveQuestionStatus) })
+  status: LiveQuestionStatus;
+
+  @Prop({ type: Object })
+  answer: Answer;
+
+  @Prop()
+  answeredAt: Date;
 }
 
 export const LiveQuestionSchema = SchemaFactory.createForClass(LiveQuestion);
